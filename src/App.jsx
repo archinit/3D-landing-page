ee615e266adca53e1461ed28c87d8c45954caa85
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-05 22:31:15 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 export default function App(){
   return (
-    <div className="font-sans text-center text-yellow-800 text-9xl underline">
-      hello world
-    </div>
+    <main>
+
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="" />
+
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_400px_20px_#e99b63] rotate-[-30deg]"></div>
+    </main>
   )
 }
